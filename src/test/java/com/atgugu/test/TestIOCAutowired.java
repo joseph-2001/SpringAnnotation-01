6e1cb0e50b0d46e8e69ffdc3b215c8cf8babd2bd
Administrator <Administrator@SC-201803221043>
2018-07-02 17:02:56 +0800

    2

--- src/test/java/com/atgugu/test/TestIOCAutowired.java
+++ src/test/java/com/atgugu/test/TestIOCAutowired.java
@@ -2,13 +2,8 @@ package com.atgugu.test;
 
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
 
-import com.atguigu.bean.Person;
-import com.atguigu.config.MainConfig;
 import com.atguigu.config.MainConfigAutowire;
-import com.atguigu.config.MainConfigOfLiftCycle;
-import com.atguigu.config.MainConfigProperyValues;
 import com.atguigu.dao.BookDAO;
 import com.atguigu.service.BookService;
 
