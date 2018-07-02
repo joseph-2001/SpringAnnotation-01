59fdaff2fe2f406191d1ab619bb3f469e770131b
Administrator <Administrator@SC-201803221043>
2018-07-02 14:38:10 +0800

    1

--- src/main/java/com/atguigu/bean/Dog.java
+++ src/main/java/com/atguigu/bean/Dog.java
@@ -1,5 +1,15 @@
 package com.atguigu.bean;
 
 public class Dog {
+    public Dog() {
+        System.out.println("Dog constructor...");
+    }
 
+    public void init() {
+        System.out.println("Car init ...");
+    }
+
+    public void destory() {
+        System.out.println("Car destory ...");
+    }
 }
